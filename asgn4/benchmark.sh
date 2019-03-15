d3b778d076670846af97c124c1dd03f51b77a3f6
David Trang <datrang@ucsc.edu>
2019-03-15 14:36:32 -0700

    Added benchmark

---
+++ asgn4/benchmark.sh
@@ -0,0 +1,404 @@
+echo Creating Files
+touch test1
+touch test2
+touch test3
+touch test4
+touch test5
+touch test6
+touch test7
+touch test8
+touch test9
+touch test10
+touch test11
+touch test12
+touch test13
+touch test14
+touch test15
+touch test16
+touch test17
+touch test18
+touch test19
+touch test20
+touch test21
+touch test22
+touch test23
+touch test24
+touch test25
+touch test26
+touch test27
+touch test28
+touch test29
+touch test30
+touch test31
+touch test32
+touch test33
+touch test34
+touch test35
+touch test36
+touch test37
+touch test38
+touch test39
+touch test40
+touch test41
+touch test42
+touch test43
+touch test44
+touch test45
+touch test46
+touch test47
+touch test48
+touch test49
+touch test50
+touch test51
+touch test52
+touch test53
+touch test54
+touch test55
+touch test56
+touch test57
+touch test58
+touch test59
+touch test60
+touch test61
+touch test62
+touch test63
+touch test64
+touch test65
+touch test66
+touch test67
+touch test68
+touch test69
+touch test70
+touch test71
+touch test72
+touch test73
+touch test74
+touch test75
+touch test76
+touch test77
+touch test78
+touch test79
+touch test80
+touch test81
+touch test82
+touch test83
+touch test84
+touch test85
+touch test86
+touch test87
+touch test88
+touch test89
+touch test90
+touch test91
+touch test92
+touch test93
+touch test94
+touch test95
+touch test96
+touch test97
+touch test98
+touch test99
+touch test100
+echo Writing to Files
+ls > test1
+ls > test2
+ls > test3
+ls > test4
+ls > test5
+ls > test6
+ls > test7
+ls > test8
+ls > test9
+ls > test10
+ls > test11
+ls > test12
+ls > test13
+ls > test14
+ls > test15
+ls > test16
+ls > test17
+ls > test18
+ls > test19
+ls > test20
+ls > test21
+ls > test22
+ls > test23
+ls > test24
+ls > test25
+ls > test26
+ls > test27
+ls > test28
+ls > test29
+ls > test30
+ls > test31
+ls > test32
+ls > test33
+ls > test34
+ls > test35
+ls > test36
+ls > test37
+ls > test38
+ls > test39
+ls > test40
+ls > test41
+ls > test42
+ls > test43
+ls > test44
+ls > test45
+ls > test46
+ls > test47
+ls > test48
+ls > test49
+ls > test50
+ls > test51
+ls > test52
+ls > test53
+ls > test54
+ls > test55
+ls > test56
+ls > test57
+ls > test58
+ls > test59
+ls > test60
+ls > test61
+ls > test62
+ls > test63
+ls > test64
+ls > test65
+ls > test66
+ls > test67
+ls > test68
+ls > test69
+ls > test70
+ls > test71
+ls > test72
+ls > test73
+ls > test74
+ls > test75
+ls > test76
+ls > test77
+ls > test78
+ls > test79
+ls > test80
+ls > test81
+ls > test82
+ls > test83
+ls > test84
+ls > test85
+ls > test86
+ls > test87
+ls > test88
+ls > test89
+ls > test90
+ls > test91
+ls > test92
+ls > test93
+ls > test94
+ls > test95
+ls > test96
+ls > test97
+ls > test98
+ls > test99
+ls > test100
+echo Reading Files
+cat test1
+cat test2
+cat test3
+cat test4
+cat test5
+cat test6
+cat test7
+cat test8
+cat test9
+cat test10
+cat test11
+cat test12
+cat test13
+cat test14
+cat test15
+cat test16
+cat test17
+cat test18
+cat test19
+cat test20
+cat test21
+cat test22
+cat test23
+cat test24
+cat test25
+cat test26
+cat test27
+cat test28
+cat test29
+cat test30
+cat test31
+cat test32
+cat test33
+cat test34
+cat test35
+cat test36
+cat test37
+cat test38
+cat test39
+cat test40
+cat test41
+cat test42
+cat test43
+cat test44
+cat test45
+cat test46
+cat test47
+cat test48
+cat test49
+cat test50
+cat test51
+cat test52
+cat test53
+cat test54
+cat test55
+cat test56
+cat test57
+cat test58
+cat test59
+cat test60
+cat test61
+cat test62
+cat test63
+cat test64
+cat test65
+cat test66
+cat test67
+cat test68
+cat test69
+cat test70
+cat test71
+cat test72
+cat test73
+cat test74
+cat test75
+cat test76
+cat test77
+cat test78
+cat test79
+cat test80
+cat test81
+cat test82
+cat test83
+cat test84
+cat test85
+cat test86
+cat test87
+cat test88
+cat test89
+cat test90
+cat test91
+cat test92
+cat test93
+cat test94
+cat test95
+cat test96
+cat test97
+cat test98
+cat test99
+cat test100
+echo Removing Files
+rm test1
+rm test2
+rm test3
+rm test4
+rm test5
+rm test6
+rm test7
+rm test8
+rm test9
+rm test10
+rm test11
+rm test12
+rm test13
+rm test14
+rm test15
+rm test16
+rm test17
+rm test18
+rm test19
+rm test20
+rm test21
+rm test22
+rm test23
+rm test24
+rm test25
+rm test26
+rm test27
+rm test28
+rm test29
+rm test30
+rm test31
+rm test32
+rm test33
+rm test34
+rm test35
+rm test36
+rm test37
+rm test38
+rm test39
+rm test40
+rm test41
+rm test42
+rm test43
+rm test44
+rm test45
+rm test46
+rm test47
+rm test48
+rm test49
+rm test50
+rm test51
+rm test52
+rm test53
+rm test54
+rm test55
+rm test56
+rm test57
+rm test58
+rm test59
+rm test60
+rm test61
+rm test62
+rm test63
+rm test64
+rm test65
+rm test66
+rm test67
+rm test68
+rm test69
+rm test70
+rm test71
+rm test72
+rm test73
+rm test74
+rm test75
+rm test76
+rm test77
+rm test78
+rm test79
+rm test80
+rm test81
+rm test82
+rm test83
+rm test84
+rm test85
+rm test86
+rm test87
+rm test88
+rm test89
+rm test90
+rm test91
+rm test92
+rm test93
+rm test94
+rm test95
+rm test96
+rm test97
+rm test98
+rm test99
+rm test100
